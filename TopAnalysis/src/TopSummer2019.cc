fb46e062b8b57aa673c65395cc0ee8d999afeb9d
Leonora Irene Verveld <lverveld@lxplus706.cern.ch>
2019-07-22 14:14:29 +0200

    update TopSummer2019 file to Pedro's version for lowlumi

--- TopAnalysis/src/TopSummer2019.cc
+++ TopAnalysis/src/TopSummer2019.cc
@@ -34,17 +34,17 @@ void RunTopSummer2019(const TString in_fname,
   /////////////////////
   // INITIALIZATION //
   ///////////////////
-
+  
   bool isLowPUrun(false);
   if(in_fname.Contains("2017H")) {
     isLowPUrun=true;
     cout << "Running with low PU run settings" << endl;
   }
 
-  //preselection cuts
-  float minLeptonPt(isLowPUrun ? 20. : 27);
+  //preselection cuts to apply
+  float minLeptonPt( isLowPUrun ? 20. : 27);
   size_t minJetMultiplicity(4);
-  
+
   //CORRECTIONS: LUMINOSITY+PILEUP
   LumiTools lumi(era,genPU);
   std::map<Int_t,Float_t> lumiPerRun=lumi.lumiPerRun();
@@ -71,12 +71,11 @@ void RunTopSummer2019(const TString in_fname,
   fOut->cd();
   HistTool ht;
   ht.setNsyst(0);
-  ht.addHist("nvtx",         new TH1F("nvtx",        ";Vertex multiplicity;Events",4,0,100));
-  ht.addHist("mlb",          new TH1F("mlb",         ";m(l,b) [GeV];Events",20,0,250));
-  ht.addHist("mlnjets",      new TH1F("mlnjets",     ";m(l,neutrino,jets) [GeV];Events",40,0,2500)); //invariant mass of lepton, neutrino, and jets
+  ht.addHist("nvtx",         new TH1F("nvtx",        ";Vertex multiplicity;Events",50,0,100));
+  ht.addHist("mlb",          new TH1F("mlb",         ";m(l,b) [GeV];Events",20,0,250)); 
   ht.addHist("nprotons",     new TH1F("nprotons",    ";Proton multiplicity; Events",6,0,6) );
   ht.addHist("csi",          new TH1F("csi",         ";#xi = #deltap/p; Events",50,0,0.3) );
-  ht.addHist("x",            new TH1F("x",           "x [cm]; Events",50,0,25));
+  ht.addHist("x",            new TH1F("x",           ";x  [cm]; Events",50,0,25) );
   ht.addHist("ratevsrun",    new TH1F("ratevsrun",   ";Run number; #sigma [pb]",int(lumiPerRun.size()),0,float(lumiPerRun.size())));
   int i=0;
   for(auto key : lumiPerRun) {
@@ -84,9 +83,15 @@ void RunTopSummer2019(const TString in_fname,
     ht.getPlots()["ratevsrun"]->GetXaxis()->SetBinLabel(i,Form("%d",key.first));
   }
 
+
   //INPUT
   MiniEvent_t ev;
   TFile *f = TFile::Open(in_fname);
+  if(f==NULL || f->IsZombie()) {
+    cout << "Corrupted or missing file " << in_fname << endl;
+    return;
+  }
+
   TH1 *triggerList=(TH1 *)f->Get("analysis/triggerList");
   TTree *t = (TTree*)f->Get("analysis/data");
   attachToMiniEventTree(t,ev,true);
@@ -102,16 +107,16 @@ void RunTopSummer2019(const TString in_fname,
   //select mu+>=4 jets events triggered by a single muon trigger
   for (Int_t iev=0;iev<nentries;iev++)
     {
-      t->GetEntry(iev); //get current event
+      t->GetEntry(iev);
       if(iev%1000==0) { printf("\r [%3.0f%%] done", 100.*(float)iev/(float)nentries); fflush(stdout); }
 
       //trigger
       bool hasMTrigger(false);
       if(era.Contains("2016")) hasMTrigger=(selector.hasTriggerBit("HLT_IsoMu24_v", ev.triggerBits) );     
       if(era.Contains("2017")) {
-	if(isLowPUrun) hasMTrigger=(selector.hasTriggerBit("HLT_HIMu15_v", ev.addTriggerBits));
-	else hasMTrigger=(selector.hasTriggerBit("HLT_IsoMu27_v", ev.triggerBits));
-      }     
+        if(isLowPUrun) hasMTrigger=(selector.hasTriggerBit("HLT_HIMu15_v",  ev.addTriggerBits) );   
+        else           hasMTrigger=(selector.hasTriggerBit("HLT_IsoMu27_v", ev.triggerBits) );   
+      }
       if(!hasMTrigger) continue;
 
       //select one offline muon
@@ -119,7 +124,7 @@ void RunTopSummer2019(const TString in_fname,
       leptons = selector.selLeptons(leptons,SelectionTool::TIGHT,SelectionTool::MVA90,minLeptonPt,2.1);
       if(leptons.size()!=1) continue;
       if(leptons[0].id()!=13) continue;
-      
+
       //select jets
       btvSF.addBTagDecisions(ev);
       if(!ev.isData) btvSF.updateBTagDecisions(ev);      
@@ -161,18 +166,6 @@ void RunTopSummer2019(const TString in_fname,
 
       ht.fill("nvtx",       ev.nvtx,        evWgt, "inc");
 
-      //calculate invariant mass of the system
-      TLorentzVector lvjets(leptons[0]+met); //longitudinal momentum of neutrino not included as of yet
-      for(size_t ij=0; ij<allJets.size(); ij++)
-	{
-	  lvjets+=allJets[ij];
-	}
-      float mlnjets(lvjets.M());
-
-      //float mlnjets((leptons[0]+met+allJets[0]+allJets[1]+allJets[2]+allJets[3]).M());
-
-      ht.fill("mlnjets",mlnjets,evWgt,"invariant mass");
-
       //lepton-b systems
       for(size_t ij=0; ij<allJets.size(); ij++) 
         {
@@ -187,32 +180,31 @@ void RunTopSummer2019(const TString in_fname,
 
       //roman pots
       int nprotons23(0), nprotons123(0);
-      int ntrks(isLowPUrun ? ev.nppstrk : ev.nfwdtrk); //what is nppstrk?
-      //double nvtx_mean(27); //not currently the mean of nvtx. Improve later.
-      int v_low_cutoff(25);
-
+      int ntrks( isLowPUrun ? ev.nppstrk : ev.nfwdtrk );
       for (int ift=0; ift<ntrks; ift++) {
 
         //single pot reconstruction
         if(!isLowPUrun && ev.fwdtrk_method[ift]!=0) continue;
 
         //only near (pixels) detectors
-	const unsigned short pot_raw_id = (isLowPUrun ? ev.ppstrk_pot[ift] : ev.fwdtrk_pot[ift]);
-	if (pot_raw_id!=23 && pot_raw_id!=123) continue;            
-	
-	nprotons23 += (pot_raw_id==23);
-	nprotons123 += (pot_raw_id==123);
-	
-	float xi=(isLowPUrun ? 0. : ev.fwdtrk_xi[ift]);
-	float x=(isLowPUrun? ev.ppstrk_x[ift] : 0.);
-	std::vector<TString> tags={"inc",ev.nvtx<v_low_cutoff ? "v_low" : "not_v_low"};
-	ht.fill("csi",xi,evWgt,tags);
-	ht.fill("x",x,evWgt,tags);
+        const unsigned short pot_raw_id = (isLowPUrun ? ev.ppstrk_pot[ift] : ev.fwdtrk_pot[ift]);
+        if (pot_raw_id!=23 && pot_raw_id!=123) continue;            
+          
+        nprotons23 += (pot_raw_id==23);
+        nprotons123 += (pot_raw_id==123);
+        std::vector<TString> tags={"inc",Form("rp%d",pot_raw_id)};
+
+        float xi= (isLowPUrun ? 0.               : ev.fwdtrk_xi[ift]);
+        float x=  (isLowPUrun ? ev.ppstrk_x[ift] :  0. );
+
+        ht.fill("csi",     xi,                    evWgt,tags);
+        ht.fill("x",       x,                     evWgt,tags);
+        ht.fill("nprotons",nprotons23+nprotons123,evWgt,"inc");
+        ht.fill("nprotons",nprotons23,            evWgt,"rp23");
+        ht.fill("nprotons",nprotons123,           evWgt,"rp123");
       }
-      ht.fill("nprotons",nprotons23+nprotons123,evWgt,"inc");
-      ht.fill("nprotons",nprotons23,            evWgt,"rp23");
-      ht.fill("nprotons",nprotons123,           evWgt,"rp123");
-      }
+ 
+    }
   
   //close input file
   f->Close();
